99ac5f4ef4aff05bba68ec3e37c739234cdb4df3
Umera <umeramalek796@gmail.com>
2021-12-02 14:05:30 -0800

    added resolver user query

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -25,3 +25,17 @@ const resolvers = {
     product: async (parent, { _id }) => {
       return await Product.findById(_id).populate('category');
     },
+    user: async (parent, args, context) => {
+        if (context.user) {
+          const user = await User.findById(context.user._id).populate({
+            path: 'orders.products',
+            populate: 'category'
+          });
+  
+          user.orders.sort((a, b) => b.purchaseDate - a.purchaseDate);
+  
+          return user;
+        }
+  
+        throw new AuthenticationError('Not logged in');
+      },
