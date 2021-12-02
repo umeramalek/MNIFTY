2302238c370c1a26ced833cdf1a7eaeac6dfb7fb
Umera <umeramalek796@gmail.com>
2021-12-02 14:06:31 -0800

    added resolvers order query

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -39,3 +39,16 @@ const resolvers = {
   
         throw new AuthenticationError('Not logged in');
       },
+      order: async (parent, { _id }, context) => {
+        if (context.user) {
+          const user = await User.findById(context.user._id).populate({
+            path: 'orders.products',
+            populate: 'category'
+          });
+  
+          return user.orders.id(_id);
+        }
+  
+        throw new AuthenticationError('Not logged in');
+      },
+      
