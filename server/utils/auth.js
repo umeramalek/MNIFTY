0b383d8bb58b0f21598fb826e15d8aad809fc321
Tyler B Yeager <wow_d2@hotmail.com>
2021-12-02 13:22:14 -0800

    add auth.js code. Use jsonwebtokens, verify token and get user data. export for use on server.js

---
+++ server/utils/auth.js
@@ -0,0 +1,38 @@
+const jwt = require('jsonwebtoken');
+
+// set token secret and expiration date
+const secret = 'mysecretsshhhhh';
+const expiration = '2h';
+
+module.exports = {
+  
+  authMiddleware: function ({req}) {
+    // allows token to be sent via  req.query or headers
+    let token = req.body.token || req.headers.authorization || req.query.token;
+
+    // ["Bearer", "<tokenvalue>"]
+    if (req.headers.authorization) {
+      token = token.split(' ').pop().trim();
+    }
+
+    if (!token) {
+      return req;
+    }
+
+    // verify token and get user data out of it
+    try {
+      const { data } = jwt.verify(token, secret, { maxAge: expiration });
+      req.user = data;
+    } catch {
+      console.log('Invalid token');
+      
+    }
+
+    return req;
+  },
+  signToken: function ({ username, email, _id }) {
+    const payload = { username, email, _id };
+
+    return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
+  },
+};
