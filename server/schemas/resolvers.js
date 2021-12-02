b92637def3b70446a0610a94eb9f7914dd1ca896
Umera <umeramalek796@gmail.com>
2021-12-02 14:08:38 -0800

    added mutation query in resolver with authentication error

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -86,3 +86,55 @@ const resolvers = {
   
         return { session: session.id };
       }
+    },
+    Mutation: {
+      addUser: async (parent, args) => {
+        const user = await User.create(args);
+        const token = signToken(user);
+  
+        return { token, user };
+      },
+      addOrder: async (parent, { products }, context) => {
+        console.log(context);
+        if (context.user) {
+          const order = new Order({ products });
+  
+          await User.findByIdAndUpdate(context.user._id, { $push: { orders: order } });
+  
+          return order;
+        }
+  
+        throw new AuthenticationError('Not logged in');
+      },
+      updateUser: async (parent, args, context) => {
+        if (context.user) {
+          return await User.findByIdAndUpdate(context.user._id, args, { new: true });
+        }
+  
+        throw new AuthenticationError('Not logged in');
+      },
+      updateProduct: async (parent, { _id }) => {
+  
+        return await Product.findByIdAndUpdate(_id);
+      },
+      login: async (parent, { email, password }) => {
+        const user = await User.findOne({ email });
+  
+        if (!user) {
+          throw new AuthenticationError('Incorrect credentials');
+        }
+  
+        const correctPw = await user.isCorrectPassword(password);
+  
+        if (!correctPw) {
+          throw new AuthenticationError('Incorrect credentials');
+        }
+  
+        const token = signToken(user);
+  
+        return { token, user };
+      }
+    }
+  };
+  
+  module.exports = resolvers;
