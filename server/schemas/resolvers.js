f01839732cab6c9f6dbb2958a9373f849dbd411b
Umera <umeramalek796@gmail.com>
2021-12-02 14:03:50 -0800

    exported resolver dependies

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,23 @@
+const { AuthenticationError } = require('apollo-server-express');
+const { User, Product, Category, Order } = require('../models');
+const { signToken } = require('../utils/auth');
+const stripe = require('stripe')('sk_test_4eC39HqLyjWDarjtT1zdp7dc');
+
+const resolvers = {
+    Query: {
+      categories: async () => {
+        return await Category.find();
+      },
+      products: async (parent, { category, name }) => {
+        const params = {};
+  
+        if (category) {
+          params.category = category;
+        }
+  
+        if (name) {
+          params.name = {
+            $regex: name
+          };
+        }
+  
