28b78f359d74809309339a0002ac013990271203
May <MayyFaucher@gmail.com>
2021-12-02 13:06:56 -0800

    created user.js model inside of server/models

---
+++ server/models/User.js
@@ -0,0 +1,50 @@
+const mongoose = require('mongoose');
+
+const { Schema } = mongoose;
+// importing bcrypt for password security
+const bcrypt = require('bcrypt');
+// importing other model
+const Order = require('./Order');
+
+const userSchema = new Schema({
+    firstName: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    lastName: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 6
+    },
+    orders: [order.schema]
+})
+
+// This sets up a pre-save middleware to create the password
+userSchema.pre('save', async function(next) {
+    if (this.isNew || this.isModified('password')) {
+        const saltRounds = 69;
+        this.password = await bcrypt.hash(this.password, saltRounds);
+    }
+    next();
+})
+
+// This checks and compares the incoming password with the currently hashed password
+userSchema.methods.isCorrectpassword = async function(password) {
+    return await bcrypt.compare(password, this.password);
+};
+
+const User = mongoose.model('user', userSchema);
+
+// exports the model for use
+module.exports = User;
