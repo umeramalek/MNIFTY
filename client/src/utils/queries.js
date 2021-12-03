ae4843a7b525c2e07dc66891fea901da73996e0b
Tyler B Yeager <wow_d2@hotmail.com>
2021-12-02 17:42:07 -0800

    add queries for all products, categories, user information, and product checkout

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -7,7 +7,6 @@ export const QUERY_PRODUCTS = gql`
       name
       description
       price
-      quantity
       image
       category {
         _id
@@ -16,3 +15,53 @@ export const QUERY_PRODUCTS = gql`
   }
 `;
 
+export const QUERY_ALL_PRODUCTS = gql`
+  {
+    products {
+      _id
+      name
+      description
+      price
+      category {
+        name
+      }
+    }
+  }
+`;
+
+export const QUERY_CATEGORIES = gql`
+  {
+    categories {
+      _id
+      name
+    }
+  }
+`;
+
+export const QUERY_USER = gql`
+  {
+    user {
+      firstName
+      lastName
+      orders {
+        _id
+        purchaseDate
+        products {
+          _id
+          name
+          description
+          price
+          image
+        }
+      }
+    }
+  }
+`;
+
+export const QUERY_CHECKOUT = gql`
+  query getCheckout($products: [ID]!) {
+    checkout(products: $products) {
+      session
+    }
+  }
+`;
