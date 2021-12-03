7005870c242beaeeb343af24b321c17186a7c550
Tyler B Yeager <wow_d2@hotmail.com>
2021-12-02 17:39:41 -0800

    add mutations and queries files. Add Query for QUERY_PRODUCTS

---
+++ client/src/utils/queries.js
@@ -0,0 +1,18 @@
+import { gql } from '@apollo/client';
+
+export const QUERY_PRODUCTS = gql`
+  query getProducts($category: ID) {
+    products(category: $category) {
+      _id
+      name
+      description
+      price
+      quantity
+      image
+      category {
+        _id
+      }
+    }
+  }
+`;
+
