af5277c5c7f8921db291e489f38cd33c39bcce78
Umera <umeramalek796@gmail.com>
2021-12-03 22:29:37 -0800

    adde on click function

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -5,3 +5,27 @@ import { Link } from 'react-router-dom';
 import { LOGIN } from '../utils/mutations';
 import Auth from '../utils/auth';
 
+function Login(props) {
+    const [formState, setFormState] = useState({ email: "", password: "" });
+    const [login, { error }] = useMutation(LOGIN);
+
+    const handleFormSubmit = async (event) => {
+        event.preventDefault();
+        try {
+            const mutationResponse = await login({
+                variables: { email: formState.email, password: formState.password },
+            });
+            const token = mutationResponse.data.login.token;
+            Auth.login(token);
+        } catch (err) {
+            console.log(error)
+        }
+    }
+
+    const handleChange = (event) => {
+        const { name, value } = event.target;
+        setFormState({
+            ...formState,
+            [name]: value,
+        });
+    };
