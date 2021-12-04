98202e69c26001440fcb8acbef8e0bd4a4f7f08d
Umera <umeramalek796@gmail.com>
2021-12-04 01:56:32 -0800

    added success function

--- client/src/pages/Success.js
+++ client/src/pages/Success.js
@@ -3,3 +3,12 @@ import { useMutation } from '@apollo/client';
 import Jumbotron from '../components/Jumbotron';
 import { ADD_ORDER } from '../utils/mutations';
 import { idbPromise } from '../utils/helpers';
+
+function Success() {
+    const [addOrder] = useMutation(ADD_ORDER);
+  
+    useEffect(() => {
+      async function saveOrder() {
+        const cart = await idbPromise('cart', 'get');
+        const products = cart.map((item) => item._id);
+  
