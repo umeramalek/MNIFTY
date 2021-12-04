c3a00f7e82a5e2bce113058d75abdd20431bc427
Tyler B Yeager <wow_d2@hotmail.com>
2021-12-03 20:52:08 -0800

    add jsx return to create detail page in browser.

--- client/src/pages/Detail.js
+++ client/src/pages/Detail.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { Link, useParams, } from "react-router-dom";
-import { useQuery } from "apollo@client";
+import { useQuery } from "@apollo/client";
 
 // import Cart from  "../components/Cart"; 
 // don't current need Cart but will in the future
@@ -19,7 +19,7 @@ import {
 import { QUERY_PRODUCTS } from "../utils/queries";
 import { idbPromise } from "../utils/IndexDB";
 
-import loading from "../assets/loading.gif";
+import loadingSpin from "../assets/loading.gif";
 
 function Detail() {
     const [state, dispatch] = useStoreContext();
@@ -89,4 +89,39 @@ function Detail() {
 
         idbPromise('cart', 'delete', { ...currentProduct });
     };
+
+
+    return (
+        <>
+          {currentProduct && cart ? (
+            <div className="container my-1">
+              <Link to="/">← Back to Products</Link>
+    
+              <h2>{currentProduct.name}</h2>
+    
+              <p>{currentProduct.description}</p>
+    
+              <p>
+                <strong>Price:</strong>{currentProduct.price} MUT Tokens {' '}
+                <button onClick={addToCart}>Add to Cart</button>
+                <button
+                  disabled={!cart.find((p) => p._id === currentProduct._id)}
+                  onClick={removeFromCart}
+                >
+                  Remove from Cart
+                </button>
+              </p>
+    
+              <img
+                src={`/images/${currentProduct.image}`}
+                alt={currentProduct.name}
+              />
+            </div>
+          ) : null}
+          {loading ? <img src={loadingSpin} alt="loading" /> : null}
+          {/* <Cart /> */}
+        </>
+      );
 };
+
+export default Detail;
