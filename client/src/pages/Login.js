d75044b3be2ca2d00aa15ad74eef61dfc3ed7d29
Umera <umeramalek796@gmail.com>
2021-12-03 20:52:02 -0800

    added jsx for login with react strap

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,36 +1,79 @@
 import React, { useState } from 'react';
-import { Navbar, NavbarBrand,NavbarToggler,Collapse,NavItem,NavLink } from 'reactstrap';
+import { Form, FormGroup, Input, Label, Button } from 'reactstrap';
 import { useMutation } from '@apollo/client';
-
+import { Link } from 'react-router-dom';
 import { LOGIN } from '../utils/mutations';
 import Auth from '../utils/auth';
 
 function Login(props) {
-    const [formState, setFormState] = useState({email:"", password:""});
-    const [login, {error}] = useMutation(LOGIN);
+    const [formState, setFormState] = useState({ email: "", password: "" });
+    const [login, { error }] = useMutation(LOGIN);
 
     const handleFormSubmit = async (event) => {
         event.preventDefault();
         try {
             const mutationResponse = await login({
-            variables: {email: formState.email, password:formState.password},
+                variables: { email: formState.email, password: formState.password },
             });
-            const token= mutationResponse.data.login.token;
+            const token = mutationResponse.data.login.token;
             Auth.login(token);
-        } catch(err){
+        } catch (err) {
             console.log(error)
         }
     }
 
     const handleChange = (event) => {
-        const {name, value} = event.target;
+        const { name, value } = event.target;
         setFormState({
-            ...formState, 
+            ...formState,
             [name]: value,
         });
     };
 
     return (
+        <div className="container my-1">
+            <Link to="/signup"> Sign Up </Link>
+
+            <h2>Login</h2>
+
+            <Form inline onSubmit={handleFormSubmit}>
+                <FormGroup floating>
+                    <Input
+                        id="exampleEmail"
+                        name="email"
+                        placeholder="Email"
+                        type="email"
+                        onChange={handleChange}
+                    />
+                    <Label for="exampleEmail">
+                        Email
+                    </Label>
+                </FormGroup>
+                {' '}
+                <FormGroup floating>
+                    <Input
+                        id="examplePassword"
+                        name="password"
+                        placeholder="Password"
+                        type="password"
+                        onChange={handleChange}
+                    />
+                    <Label for="examplePassword">
+                        Password
+                    </Label>
+                </FormGroup>
+                {' '}
+                {error ? (
+                    <div>
+                        <p className="error-text">The provided credentials are incorrect</p>
+                    </div>
+                ) : null}
+                <Button>
+                    Submit
+                </Button>
+            </Form>
+        </div>
+
 
     )
 
