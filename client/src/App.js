a48648edaaf72261913876e9ad2d4a7e419640d2
Umera <umeramalek796@gmail.com>
2021-12-04 01:38:09 -0800

    uncommented signup code in app.js

--- client/src/App.js
+++ client/src/App.js
@@ -14,7 +14,7 @@ import Main from './pages/Main';
 import Login from './pages/Login';
 import Nav from './components/Nav';
 import Detail from "./pages/Detail";
-// import Signup from './pages/Signup';
+import Signup from './pages/Signup';
 // import Success from './pages/Success';
 import NoMatch from './pages/NoMatch';
 // import OrderHistory from './pages/OrderHistory';
@@ -53,8 +53,8 @@ function App() {
             <Switch>
               <Route exact path="/" component={Main} />
               <Route exact path="/login" component={Login} />
-              {/* <Route exact path="/signup" component={Signup} />
-              <Route exact path="/success" component={Success} />
+              <Route exact path="/signup" component={Signup} />
+              {/* <Route exact path="/success" component={Success} />
               <Route exact path="/orderHistory" component={OrderHistory} /> */}
               <Route exact path="/products/:id" component={Detail} />
               <Route component={NoMatch} />
