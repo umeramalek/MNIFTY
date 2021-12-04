19404442a4b416f46647001cd809978f71f30d11
Umera <umeramalek796@gmail.com>
2021-12-04 01:28:08 -0800

    added state finctionality

--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -3,3 +3,31 @@ import { Link } from 'react-router-dom';
 import { useMutation } from '@apollo/client';
 import Auth from '../utils/auth';
 import { ADD_USER } from '../utils/mutations';
+import { Form, FormGroup, Input, Label, Button } from 'reactstrap';
+
+function Signup(props) {
+    const [formState, setFormState] = useState({ email: "", password: "" });
+
+    const handleFormSubmit = async (event) => {
+        event.preventDefault();
+        const mutationResponse = await addUser({
+            variables: {
+                email: formState.email,
+                password: formState.password,
+                firstName: formState.firstName,
+                lastName: formState.lastName,
+            },
+        }),
+        const token = mutationResponse.data.addUser.token;
+        Auth.login(token);
+    };
+
+    const handleChange = (event) => {
+        const { name, value } = event.target;
+        setFormState({
+            ...formState,
+            [name]: value,
+        });
+    };
+
+    
