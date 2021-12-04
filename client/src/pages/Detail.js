f7bb3f58388a8a875ab26ebdcf49ad095140d045
Tyler B Yeager <wow_d2@hotmail.com>
2021-12-03 20:20:31 -0800

    added use effect to retrieve product information from global store, server, or indexDB

--- client/src/pages/Detail.js
+++ client/src/pages/Detail.js
@@ -21,3 +21,37 @@ import { idbPromise } from "../utils/IndexDB";
 
 import loading from "../assets/loading.gif";
 
+function Detail() {
+    const [state, dispatch] = useStoreContext();
+    const { id } = useParams();
+    const [currentProduct, setCurrentProduct] = useState({});
+    const { loading, data } = useQuery(QUERY_PRODUCTS);
+    const { products, cart } = state;
+
+
+
+    useEffect(() => {
+        // checks to see if a product already exists within the global store
+        if (products.length) {
+            setCurrentProduct(products.find((product) => product._id === id));
+        } else if (data) {
+        // if the product doesn't exist in the global store then it will check the server data
+            dispatch({
+                type: UPDATE_PRODUCTS,
+                products: data.products,
+            });
+
+            data.products.forEach((product) => {
+                idbPromise('products', 'put', product);
+            });
+        } else if (!loading) {
+        // if it doesn't exist in global store or server then it will pull the caache from indexDB
+            idbPromise('products', 'get').then((indexedProducts) => {
+                dispatch({
+                    type: UPDATE_PRODUCTS,
+                    products: indexedProducts,
+                });
+            });
+        }
+    }, [products, data, loading, dispatch, id]);
+};
