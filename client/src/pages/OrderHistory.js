015c9387be3251570f9d28eadb612f28ff23027a
Umera <umeramalek796@gmail.com>
2021-12-04 01:45:08 -0800

    created a order history function

--- client/src/pages/OrderHistory.js
+++ client/src/pages/OrderHistory.js
@@ -3,3 +3,12 @@ import { Link } from 'react-router-dom';
 
 import { useQuery } from '@apollo/client';
 import { QUERY_USER } from '../utils/queries';
+
+
+function OrderHistory() {
+    const { data } = useQuery(QUERY_USER);
+    let user;
+  
+    if (data) {
+      user = data.user;
+    }
